2c243fe61f99cc0d20bd578ef02b8cb5bc5706b7
Aaron Jones <jones@rmrdevelopment.com>
2018-12-27 16:02:31 -0500

    fixed site url

--- build/25.js
+++ build/25.js
@@ -114,7 +114,7 @@ var CoreLoginSitePage = /** @class */ (function () {
         this.displayAsButtons = false;
         this.showKeyboard = false;
         this.showKeyboard = !!navParams.get('showKeyboard');
-        var url = 'http://gsmoodle.rmrdevelopment.com';
+        var url = 'http://gsmoodledev.rmrdevelopment.com';
         // Load fixed sites if they're set.
         if (this.loginHelper.hasSeveralFixedSites()) {
             this.fixedSites = this.loginHelper.getFixedSites();
